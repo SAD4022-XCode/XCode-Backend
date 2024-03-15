41096db22bac805de3f125a21bca7728af9d7f56
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-15 17:11:24 +0330

    implemented serializers

---
+++ src/service/serializers/UserSerializer.py
@@ -0,0 +1,11 @@
+from djoser.serializers import UserSerializer as BaseUserSerializer
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = [
+            'id',
+            'username',
+            'email',
+            'first_name',
+            'last_name'
+        ]
