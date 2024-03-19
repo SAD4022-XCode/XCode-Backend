f70f69c61cb1c2d7084bb647460e029348c163eb
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-19 19:16:51 +0330

    chore: registered urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,8 @@
-# from django.urls import path
-# from rest_framework import routers
-# from .views import AttendeeProfileViewSet
+from django.urls import path
+from rest_framework import routers
+from .views import AttendeeProfileViewSet
 
-# router = routers.DefaultRouter()
+router = routers.DefaultRouter()
 
-# router.register('auth', AttendeeProfileViewSet, basename = 'auth')
-# urlpatterns = router.urls
+router.register('auth', AttendeeProfileViewSet, basename = 'auth')
+urlpatterns = router.urls
--- src/eventify/urls.py
+++ src/eventify/urls.py
@@ -21,6 +21,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('login/', include('api.urls'))
-    path('auth/', include('djoser.urls'))
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt'))
 ]
