fcc7e8f59a898d366b080e23e842d7c7e5be4da9
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-25 19:30:41 +0330

    feat: registered URLs

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
 from rest_framework import routers
-from .views import AttendeeProfileViewSet
+from .views import *  
 
 router = routers.DefaultRouter()
 
-router.register('auth', AttendeeProfileViewSet, basename = 'auth')
+router.register('auth', UserProfileViewSet, basename = 'auth')
+router.register('list', UserListViewSet.UserListViewSet, basename = 'list')
 urlpatterns = router.urls
+
--- src/eventify/urls.py
+++ src/eventify/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
@@ -22,5 +25,8 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt'))
+    path('auth/', include('djoser.urls.jwt')),
+    path('account/', include('api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
