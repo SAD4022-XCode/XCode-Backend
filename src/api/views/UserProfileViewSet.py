36c70c2a0d7a75685f4bc2c201f8e752ffcc523d
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-03-26 23:03:22 +0330

    feat: added PATCH method support

--- src/api/views/UserProfileViewSet.py
+++ src/api/views/UserProfileViewSet.py
@@ -1,32 +1,47 @@
+from django.http import HttpRequest
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import permissions
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 
 from data.models import UserProfile
-from service.serializers import UserProfileSerializer
+from service import serializers
 
 class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.select_related('user').all()
-    serializer_class = UserProfileSerializer
+    serializer_class = serializers.UserProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [JSONParser, MultiPartParser, FormParser]
 
     #Implement PATCH
 
-    @action(detail = False, methods = ['GET', 'PUT'], permission_classes = [permissions.IsAuthenticated])
-    def me(self, request):
+    @action(detail = False, methods = ['GET', 'PUT', 'PATCH'], permission_classes = [permissions.IsAuthenticated])
+    def me(self, request: HttpRequest):
         profile = UserProfile.objects.get(user_id = request.user.id)
 
         if request.method == 'GET':
-            serializer = UserProfileSerializer(profile)
+            serializer = serializers.UserProfileSerializer(profile)
             return Response(serializer.data)
+                    
         elif request.method == 'PUT':
-            serializer = UserProfileSerializer(profile, data = request.data)
+            serializer = serializers.UserProfileSerializer(profile, data = request.data)
+            serializer.is_valid(raise_exception = True)
+            serializer.save()
+
+            return Response(serializer.data)
+        
+        elif request.method == 'PATCH':
+            serializer = serializers.UserProfileSerializer(profile, 
+                                                           data = request.data, 
+                                                           partial = True)
             serializer.is_valid(raise_exception = True)
             serializer.save()
             return Response(serializer.data)
+            # self.partial_update(request)
+
+    # def partial_update(self, request, *args, **kwargs):
+    #     kwargs['partial'] = True
+    #     return self.update(request, *args, **kwargs)
+
     
-    def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
