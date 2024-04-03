5411824159998ba267766a02514aca5ee3410f45
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-03 20:55:00 +0330

    refactor: modified fields

--- src/service/serializers/UserSerializer.py
+++ src/service/serializers/UserSerializer.py
@@ -1,8 +1,14 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer
 from rest_framework import serializers
 
+from data.models import User
+
 class UserSerializer(BaseUserSerializer):
     id = serializers.IntegerField(read_only = True)
+    first_name = serializers.CharField(read_only = True)
+    last_name = serializers.CharField(read_only = True)
+    email = serializers.CharField(read_only = True)
+    username = serializers.CharField(read_only = False)
 
     class Meta(BaseUserSerializer.Meta):
         fields = [
@@ -12,3 +18,11 @@ class UserSerializer(BaseUserSerializer):
             'username',
             'email'
         ]
+
+    def update(self, instance, validated_data):
+        if ('username' in validated_data):
+            new_username = validated_data['username']
+            setattr(instance, User.USERNAME_FIELD, new_username)
+            instance.save()
+
+        return super().update(instance, validated_data)
