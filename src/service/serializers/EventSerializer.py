0e9e09cce4445509a272a257bbf35e0354654b4e
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-23 17:59:09 +0330

    feat: implemented event serializers

---
+++ src/service/serializers/EventSerializer.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from data.models import Event, OnlineEvent, InPersonEvent
+
+class EventSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Event
+        fields = [
+            "title",
+            "category",
+            "description",
+            "starts",
+            "ends",
+            "attendance",
+            "maximum_tickets",
+            "remaining_tickets",
+            "is_paid",
+            "ticket_price",
+            "organizer_phone",
+            "organizer_SSN",
+            "photo",
+        ]
+
+class OnlineEventSerializer(serializers.ModelSerializer):
+    event = EventSerializer(many = False, partial = True)
+    
+    class Meta:
+        model = OnlineEvent
+        fields = [
+            "event",
+            "url",
+        ]
+
+class InPersonEventSerializer(serializers.ModelSerializer):
+    event = EventSerializer(many = False, partial = True)
+
+    class Meta:
+        model = InPersonEvent
+        fields = [
+            "event",
+            "province",
+            "city",
+            "location_lat",
+            "location_lon",
+        ]
