1a3eb2c659bb71515d483885c21abf1e491da1b9
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-23 17:57:51 +0330

    chore: import

--- src/data/models/__init__.py
+++ src/data/models/__init__.py
@@ -1,2 +1,11 @@
 from .User import User
 from .UserProfile import UserProfile
+from .Event import Event, OnlineEvent, InPersonEvent
+
+__all__ = [
+    User,
+    UserProfile,
+    Event,
+    OnlineEvent,
+    InPersonEvent,
+]
--- src/service/serializers/__init__.py
+++ src/service/serializers/__init__.py
@@ -2,3 +2,14 @@ from .UserProfileSerializer import UserProfileSerializer
 from .ProfilePictureSerializer import ProfilePictureSerializer
 from .UserCreateSerializer import UserCreateSerializer
 from .UserSerializer import UserSerializer
+from .EventSerializer import EventSerializer, OnlineEventSerializer, InPersonEventSerializer
+
+__all__ = [
+    UserProfileSerializer,
+    ProfilePictureSerializer,
+    UserCreateSerializer,
+    UserSerializer,
+    EventSerializer,
+    OnlineEventSerializer,
+    InPersonEventSerializer,
+]
