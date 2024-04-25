60b5d68ac7f41a7c563be0c599f3cc99de8aa476
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-25 23:27:36 +0330

    feat: implement crud + api

--- src/api/views/EventViewSet.py
+++ src/api/views/EventViewSet.py
@@ -2,43 +2,47 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from django.http import HttpRequest, FileResponse
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from django.http import HttpRequest, FileResponse
 
 from data.models import Event
-from service.serializers import EventSerializer, InPersonEventSerializer, OnlineEventSerializer
+from service.serializers import EventSerializer, CreateEventSerializer, InPersonEventSerializer, OnlineEventSerializer
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     parser_classes = [JSONParser, FormParser, MultiPartParser]
 
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+    @action(detail = False, methods = ['GET'])
+    def all_events(self, request, *args, **kwargs):
+        serializer = EventSerializer(self.queryset, many = True)
+        return Response(serializer.data)
     
     def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
+        data = request.data
+        event = self.queryset.get(id = data.get("id"))
     
-    @action(methods = ['POST'], permission_classes = [IsAuthenticated])
-    def create(self, request: HttpRequest):
-        if request.data.get("attendance") == 'I':
-            serializer = InPersonEventSerializer(data = request.data,
-                                                 context = {"user_id": request.user.id})
+    @action(detail = False, methods = ['POST'], permission_classes = [IsAuthenticated])
+    def create_event(self, request: HttpRequest):
+        event_data = request.data
+
+        if event_data["event"].get("attendance") == 'I':
+            serializer = InPersonEventSerializer(data = event_data,
+                                                 context = {"user_id": request.user.id})                                   
             serializer.is_valid(raise_exception = True)
-            event = serializer.save()
-            serializer = InPersonEventSerializer(event)
+            # serializer.save()
             return Response(serializer.data)
         
-        elif request.data.get("attendance") == 'O':
-            serializer = OnlineEventSerializer(data = request.data,
+        elif event_data["event"].get("attendance") == 'O':
+            serializer = OnlineEventSerializer(data = event_data,
                                                context = {"user_id": request.user.id})
             serializer.is_valid(raise_exception = True)
-            event = serializer.save()
-            serializer = OnlineEventSerializer(event)
+            # serializer.save()
             return Response(serializer.data) 
         
-    @action(methods = ["PUT"], permission_classes = [IsAuthenticated])
-    def update(self, request: HttpRequest):
+    @action(detail = False, methods = ["PUT"], permission_classes = [IsAuthenticated])
+    def update_event(self, request: HttpRequest):
         if request.data.get("attendance") == 'I':
             serializer = InPersonEventSerializer(data = request.data,
                                                  partial = True,
@@ -57,8 +61,8 @@ class EventViewSet(ModelViewSet):
             serializer = OnlineEventSerializer(event)
             return Response(serializer.data) 
     
-    @action(methods = ['DELETE'], permission_classes = [IsAuthenticated])
-    def destroy(self, request, *args, **kwargs):
+    @action(detail = False, methods = ['DELETE'], permission_classes = [IsAuthenticated])
+    def delete_event(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
         
 
