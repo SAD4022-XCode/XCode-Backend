6bdffd31a1d7e08158e7160e5032f6cf798db183
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-25 23:26:33 +0330

    chore: imports

--- src/service/serializers/__init__.py
+++ src/service/serializers/__init__.py
@@ -2,7 +2,7 @@ from .UserProfileSerializer import UserProfileSerializer
 from .ProfilePictureSerializer import ProfilePictureSerializer
 from .UserCreateSerializer import UserCreateSerializer
 from .UserSerializer import UserSerializer
-from .EventSerializer import EventSerializer, OnlineEventSerializer, InPersonEventSerializer
+from .EventSerializer import EventSerializer, CreateEventSerializer, OnlineEventSerializer, InPersonEventSerializer
 from .TagSerializer import TagSerializer, TaggedEvent
 
 __all__ = [
@@ -11,6 +11,7 @@ __all__ = [
     UserCreateSerializer,
     UserSerializer,
     EventSerializer,
+    CreateEventSerializer,
     OnlineEventSerializer,
     InPersonEventSerializer,
     TagSerializer,
