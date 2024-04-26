1ecf679918a38cc6ec8e242d4673dd24488ac898
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-04-26 12:55:15 +0330

    feat: implement retrieve method

--- src/api/views/EventViewSet.py
+++ src/api/views/EventViewSet.py
@@ -1,44 +1,89 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
+from rest_framework.parsers import JSONParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from django.http import HttpRequest, FileResponse
+from django.http import HttpRequest
 
-from data.models import Event
-from service.serializers import EventSerializer, CreateEventSerializer, InPersonEventSerializer, OnlineEventSerializer
+from data.models import Event, InPersonEvent, OnlineEvent
+from service.serializers import EventSerializer, CreateEventSerializer, EventInfoSerializer, InPersonEventSerializer, OnlineEventSerializer
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    parser_classes = [JSONParser, FormParser, MultiPartParser]
+    parser_classes = [JSONParser, MultiPartParser]
+
+    serializer_action_classes = {
+        "create_event": CreateEventSerializer,
+        "all_events": EventSerializer,
+        "update": EventInfoSerializer,
+        "partial_update": EventInfoSerializer,
+        "retrieve": EventInfoSerializer,
+    }
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except(KeyError, AttributeError):
+            return super().get_serializer_class()
 
     @action(detail = False, methods = ['GET'])
     def all_events(self, request, *args, **kwargs):
-        serializer = EventSerializer(self.queryset, many = True)
+        serializer = self.get_serializer(self.queryset, many = True)
         return Response(serializer.data)
-    
-    def retrieve(self, request, *args, **kwargs):
-        data = request.data
-        event = self.queryset.get(id = data.get("id"))
+
+    # @action(detail = False, methods = ['POST'])
+    def retrieve(self, request: HttpRequest, *args, **kwargs):
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+
+        lookup = filter_kwargs.get("pk")
+
+        instance = Event.objects.get(pk = filter_kwargs.get("pk"))
+
+        if instance.attendance == "I":
+            obj = InPersonEvent.objects.select_related("event").get(event_id = instance.id)
+            event_serializer = self.get_serializer(obj)
+            return Response(event_serializer.data)
+        
+        elif instance.attendance == "O":
+            obj = OnlineEvent.objects.select_related("event").get(event_id = instance.id)
+            serializer = self.get_serializer(obj)
+            return Response(serializer.data)
+
+        return Response(status = status.HTTP_404_NOT_FOUND)
     
     @action(detail = False, methods = ['POST'], permission_classes = [IsAuthenticated])
     def create_event(self, request: HttpRequest):
-        event_data = request.data
+        serializer_class = self.get_serializer
+        data = request.data
+        event_data = {"event": {}}
 
-        if event_data["event"].get("attendance") == 'I':
+        for key in data:
+            if key in EventSerializer().get_fields().keys():
+                event_data["event"][key] = data[key]
+
+        if event_data["event"]["attendance"] == 'I':
+            serializer = InPersonEventSerializer()
+            for key in data:
+                if (key in serializer.get_fields().keys()):
+                    event_data[key] = data[key]
             serializer = InPersonEventSerializer(data = event_data,
                                                  context = {"user_id": request.user.id})                                   
             serializer.is_valid(raise_exception = True)
-            # serializer.save()
+            serializer.save()
             return Response(serializer.data)
         
-        elif event_data["event"].get("attendance") == 'O':
-            serializer = OnlineEventSerializer(data = event_data,
+        elif event_data["event"]["attendance"] == 'O':
+            serializer = OnlineEventSerializer()
+            for key in data:
+                if (key in serializer.get_fields().keys()):
+                    event_data[key] = data[key]
+            serializer = OnlineEventSerializer(data = event_data, 
                                                context = {"user_id": request.user.id})
             serializer.is_valid(raise_exception = True)
-            # serializer.save()
+            serializer.save()
             return Response(serializer.data) 
         
     @action(detail = False, methods = ["PUT"], permission_classes = [IsAuthenticated])
--- src/service/serializers/EventSerializer.py
+++ src/service/serializers/EventSerializer.py
@@ -27,53 +27,40 @@ class EventSerializer(serializers.ModelSerializer):
             "photo",
         ]
 
-class CreateEventSerializer(serializers.ModelSerializer):
-    remaining_tickets = serializers.IntegerField(read_only = True, required = False)
+class EventInfoSerializer(serializers.Serializer):
+    event = EventSerializer(read_only = True)
     url = serializers.URLField(required = False)
     province = serializers.CharField(max_length = 255, required = False)
     city = serializers.CharField(max_length = 255, required = False)
+    address = serializers.CharField(max_length = 255, required = False)
+    location_lat = serializers.DecimalField(max_digits = 9, decimal_places = 6, required = False)
+    location_lon = serializers.DecimalField(max_digits = 9, decimal_places = 6, required = False)
+
+
+
+class CreateEventSerializer(serializers.ModelSerializer):
+    event = EventSerializer(partial = True, many = False)
+    url = serializers.URLField(required = False)
+    province = serializers.CharField(max_length = 255, required = False)
+    city = serializers.CharField(max_length = 255, required = False)
+    address = serializers.CharField(max_length = 255)
     location_lat = serializers.DecimalField(max_digits = 9, decimal_places = 6, required = False)
     location_lon = serializers.DecimalField(max_digits = 9, decimal_places = 6, required = False)
 
     class Meta:
         model = models.Event
         fields = [
-            "title",
-            "category",
-            "description",
-            "starts",
-            "ends",
-            "attendance",
-            "maximum_tickets",
-            "remaining_tickets",
-            "is_paid",
-            "ticket_price",
-            "organizer_phone",
-            "organizer_SSN",
+            "event",
             "url",
             "province",
             "city",
             "address",
             "location_lat",
             "location_lon",
-            "photo",
         ]
-
-    # def create(self, validated_data):
-    #     event = EventSerializer(data = validated_data)
-    #     if (validated_data.get("attendance") == "I"):
-    #         in_person_event = InPersonEventSerializer(data = validated_data)
-    #         in_person_event.event = event
-    #         in_person_event.is_valid(raise_exception = True)
-    #         in_person_event.save()
-    #     elif (validated_data.get("attendance") == "O"):
-    #         online_event = OnlineEventSerializer(data = validated_data, event = event)
-    #         online_event.save()
-    #     event.save()
-    #     return super().create(validated_data)
-
+    
 class OnlineEventSerializer(serializers.ModelSerializer):
-    event = serializers.PrimaryKeyRelatedField(queryset = models.Event.objects.all())
+    event = EventSerializer(many = False)
     
     class Meta:
         model = models.OnlineEvent
@@ -82,8 +69,16 @@ class OnlineEventSerializer(serializers.ModelSerializer):
             "url",
         ]
 
+    def create(self, validated_data):
+        event_data = validated_data.pop("event")
+        event = models.Event.objects.create(creator_id = self.context.get("user_id"), **event_data)
+
+        in_person_event = models.OnlineEvent.objects.create(event = event, **validated_data)
+        event.save()
+        in_person_event.save()
+
 class InPersonEventSerializer(serializers.ModelSerializer):
-    event = EventSerializer(many = False, partial = True)
+    event = EventSerializer(partial = True)
 
     class Meta:
         model = models.InPersonEvent
@@ -100,18 +95,6 @@ class InPersonEventSerializer(serializers.ModelSerializer):
         event_data = validated_data.pop("event")
         event = models.Event.objects.create(creator_id = self.context.get("user_id"), **event_data)
 
-        in_person_event = models.InPersonEvent.objects.create(event = event, **validated_data)
-        event.save()
-        in_person_event.save()
-        # in_person_event.event = event
+        in_person_event = models.InPersonEvent.objects.create(event = event, **validated_data)  
 
-#         event_serializer = EventSerializer(validated_data.pop("event"))
-#         event_serializer.create(data = validated_data.pop("event"))
-#         event_serializer.is_valid(raise_exception = True)
-#         event_serializer.save()
-
-#         validated_data["event"] = event_serializer
-
-# #        self.fields["event"] = event_serializer
-        
-        # return super().create(validated_data)
+        return in_person_event
