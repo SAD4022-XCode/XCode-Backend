5dd9b3b9fdfa2e30db3fc95c81ab042c5fae2bb1
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-01 14:26:50 +0330

    fix: bug fixes

--- src/service/serializers/EventTagSerializer.py
+++ src/service/serializers/EventTagSerializer.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 
-from data.models import EventTag, Event
-from .EventSerializer import EventSerializer
+from data.models import EventTag
 
 class EventTagSerializer(serializers.ModelSerializer):
-    # event = serializers.PrimaryKeyRelatedField(queryset = Event.objects.all(), required = False)
     tag = serializers.CharField(max_length = 255, required = False)
 
     class Meta:
@@ -14,7 +12,6 @@ class EventTagSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        for tag in validated_data.values:
-            new_tag = EventTag.objects.create(tag = tag, event_id = self.context.get("event_id"))
-            new_tag.save()  
-        # return super().create(validated_data)
+        new_tag = EventTag.objects.create(event_id = self.context.get("event_id"), **validated_data)
+
+        return new_tag
