43d22ca8c1f632f14f3ea0fe0ab1afd0ab7314b6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-01 14:25:26 +0330

    fix: bug fixes

--- src/api/views/EventViewSet.py
+++ src/api/views/EventViewSet.py
@@ -6,23 +6,26 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.http import HttpRequest
 from django.utils.decorators import method_decorator
+from django.db import transaction
+from django.shortcuts import get_object_or_404
 
 from drf_yasg.utils import swagger_auto_schema
 
-from data.models import Event, InPersonEvent, OnlineEvent
+from data.models import Event
+from service.serializers import event_serializers
 from service import serializers
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
-    serializer_class = serializers.EventSerializer
+    serializer_class = event_serializers.EventSerializer
     parser_classes = [JSONParser, MultiPartParser]
 
     serializer_action_classes = {
-        "create_event": serializers.CreateEventSerializer,
-        "all_events": serializers.EventSerializer,
-        "update": serializers.EventInfoSerializer,
-        "partial_update": serializers.EventInfoSerializer,
-        "retrieve": serializers.EventInfoSerializer,
+        "create_event": event_serializers.CreateEventSerializer,
+        "list": event_serializers.EventSummarySerializer,
+        "update": event_serializers.EventDetailSerializer,
+        "partial_update": event_serializers.EventDetailSerializer,
+        "retrieve": event_serializers.EventDetailSerializer,
     }
 
     def get_serializer_class(self):
@@ -33,55 +36,54 @@ class EventViewSet(ModelViewSet):
         
     @swagger_auto_schema(operation_summary = "List of all events")
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        queryset = Event.objects.select_related("inpersonevent", "onlineevent")
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @swagger_auto_schema(operation_summary = "Event details")
+    @transaction.atomic
     def retrieve(self, request: HttpRequest, *args, **kwargs):
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
         filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-
-        instance = Event.objects.get(pk = filter_kwargs.get("pk"))
-
-        if instance.attendance == "I":
-            obj = InPersonEvent.objects.select_related("event").get(event_id = instance.id)
-            event_serializer = self.get_serializer(obj)
-            return Response(event_serializer.data)
         
-        elif instance.attendance == "O":
-            obj = OnlineEvent.objects.select_related("event").get(event_id = instance.id)
-            serializer = self.get_serializer(obj)
-            return Response(serializer.data)
-
-        return Response(status = status.HTTP_404_NOT_FOUND)
+        try:
+            instance = Event.objects \
+                .select_related("inpersonevent", "onlineevent") \
+                .get(pk = filter_kwargs.get("pk"))
+        
+        except Event.objects.model.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
     
     @swagger_auto_schema(method = "post", operation_summary = "Create a new event")
+    @transaction.atomic
     @action(detail = False, methods = ['POST'], permission_classes = [IsAuthenticated])
     def create_event(self, request: HttpRequest):
-        serializer_class = self.get_serializer
         data = request.data
-        event_data = {"event": {}}
 
-        for key in data:
-            if key in serializers.EventSerializer().get_fields().keys():
-                event_data["event"][key] = data[key]                
+        event_serializer = event_serializers.EventSerializer(data = data, 
+                                                             context = {"user_id": request.user.id})
+        event_serializer.is_valid(raise_exception = True)
+        event_serializer.save()
+        event_id = event_serializer.data.get("id")
 
-        if event_data.get("event").get("attendance") == 'I':
-            serializer = serializers.InPersonEventSerializer()
-            for key in data:
-                if (key in serializer.get_fields().keys()):
-                    event_data[key] = data[key]
-            serializer = serializers.InPersonEventSerializer(data = event_data,
-                                                 context = {"user_id": request.user.id})                                   
+        if data.get("attendance") == 'I':
+            serializer = event_serializers.InPersonEventSerializer(data = data,
+                                                 context = {"event_id": event_id})                                   
             serializer.is_valid(raise_exception = True)
             serializer.save()
         
-        elif event_data["event"]["attendance"] == 'O':
-            serializer = serializers.OnlineEventSerializer()
-            for key in data:
-                if (key in serializer.get_fields().keys()):
-                    event_data[key] = data[key]
-            serializer = serializers.OnlineEventSerializer(data = event_data, 
-                                               context = {"user_id": request.user.id})
+        elif data.get("attendance") == 'O':
+            serializer = event_serializers.OnlineEventSerializer(data = data, 
+                                               context = {"event_id": event_id})
             serializer.is_valid(raise_exception = True)
             serializer.save()
         
@@ -89,36 +91,46 @@ class EventViewSet(ModelViewSet):
             from data.models import EventTag
             tag_data = data.pop("tags", {})
             for tag in tag_data:
-                EventTag.objects.create(tag = tag_data[tag], event_id = serializer.data["event"].get("id"))
-        
+                tag_serializer = serializers.EventTagSerializer(data = {"tag": tag},
+                                                                context = {"event_id": event_id})      
+                tag_serializer.is_valid(raise_exception = True)
+                tag_serializer.save()
+                    
         return Response(serializer.data) 
         
     @action(detail = False, methods = ["PUT"], permission_classes = [IsAuthenticated])
+    @transaction.atomic
     def update_event(self, request: HttpRequest):
         if request.data.get("attendance") == 'I':
-            serializer = serializers.InPersonEventSerializer(data = request.data,
+            serializer = event_serializers.InPersonEventSerializer(data = request.data,
                                                  partial = True,
                                                  context = {"user_id": request.user.id})
             serializer.is_valid(raise_exception = True)
             event = serializer.save()
-            serializer = serializers.InPersonEventSerializer(event)
+            serializer = event_serializers.InPersonEventSerializer(event)
             return Response(serializer.data)
         
         elif request.data.get("attendance") == 'O':
-            serializer = serializers.OnlineEventSerializer(data = request.data,
+            serializer = event_serializers.OnlineEventSerializer(data = request.data,
                                                parial = True,
                                                context = {"user_id": request.user.id})
             serializer.is_valid(raise_exception = True)
             event = serializer.save()
-            serializer = serializers.OnlineEventSerializer(event)
+            serializer = event_serializers.OnlineEventSerializer(event)
             return Response(serializer.data) 
 
+    @transaction.atomic
     def destroy(self, request, *args, **kwargs):
         permission_classes = [IsAuthenticated]
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
         filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
 
-        instance = Event.objects.get(pk = filter_kwargs.get("pk"))
+        try:
+            instance = Event.objects.get(pk = filter_kwargs.get("pk"))
+
+        except Event.objects.model.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
+
         if instance.creator_id != request.user.id:
             return Response(status = status.HTTP_401_UNAUTHORIZED)
         
