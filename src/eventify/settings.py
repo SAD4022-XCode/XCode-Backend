50f3bf1b7636d38da14382dbfb3b527ed1960920
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-01 16:56:46 +0330

    fix: resolve cors error

--- src/eventify/settings.py
+++ src/eventify/settings.py
@@ -30,6 +30,10 @@ DEBUG = True
 CORS_ALLOW_CRIDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
 CORS_ALLOW_METHODS = [
   "DELETE",
   "GET",
