9139684d457dce22ac4f13b5073204f3ba6f4e56
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-02 18:34:54 +0330

    chore: imports

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -2,10 +2,12 @@ from .UserProfileViewSet import UserProfileViewSet
 from .UserListView import UserList
 from .EventViewSet import EventViewSet
 from .SwaggerSchemaView import SwaggerSchemaView
+from .EventListViewSet import EventListView
 
 __all__ = [
     UserProfileViewSet,
     EventViewSet,
     SwaggerSchemaView,
+    EventListView,
     UserList,
 ]
