0bb90c9a18c1bf905bc8474d93e893567ea0bfc3
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-14 10:17:57 +0330

    refactor(comment-serializers): modify fields

--- src/service/serializers/CreateCommentSerializer.py
+++ src/service/serializers/CreateCommentSerializer.py
@@ -3,19 +3,19 @@ from rest_framework import serializers
 from data import models
 
 class CreateCommentSerializer(serializers.ModelSerializer):
-    parent = serializers.PrimaryKeyRelatedField(queryset = models.Comment.objects.all(), required = False)
+    event = serializers.PrimaryKeyRelatedField(queryset = models.Event.objects.all())
 
     class Meta:
         model = models.Comment
         fields = [
+            "event",
             "text",
-            "parent",
         ]
 
     def create(self, validated_data):
         user_id = self.context.get("user_id")
-        event_id = self.context.get("event_id")
-        instance = models.Comment.objects.create(user_id = user_id,
-                                                 event_id = event_id,
+        parent_id = self.context.get("parent_id")
+        instance = models.Comment.objects.create(user_id = user_id, 
+                                                 parent_id = parent_id, 
                                                  **validated_data)
         return instance
