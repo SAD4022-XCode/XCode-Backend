2e9a9416c64b4b9120211b89a2519863945857e7
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-14 10:17:19 +0330

    refactor(comment-viewset): general refactoring

--- src/api/views/CommentViewSet.py
+++ src/api/views/CommentViewSet.py
@@ -1,20 +1,26 @@
+from django.http import HttpRequest
 from rest_framework import viewsets
 from rest_framework import parsers
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from drf_yasg.utils import swagger_auto_schema
 
 from data import models
 from service import serializers
+from service import pagination
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = models.Comment.objects.all()
+    queryset = models.Comment.objects.prefetch_related("children").all()
     serializer_class = serializers.CommentSerializer
     parser_classes = [parsers.JSONParser, parsers.MultiPartParser]
+    pagination_class = pagination.CustomPagination
 
     serializer_action_classes = {
-        "create" : serializers.CreateCommentSerializer
+        "create" : serializers.CreateCommentSerializer,
+        "reply" : serializers.CreateCommentSerializer,
     }
 
     def get_serializer_class(self):
@@ -33,7 +39,39 @@ class CommentViewSet(viewsets.ModelViewSet):
                                          context = {
                                             "user_id": request.user.id,
                                          })
-        serializer.is_valid(raise_exception=True)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status = status.HTTP_201_CREATED, headers = headers)
+
+    @swagger_auto_schema(operation_summary = "List of all comments")
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    @swagger_auto_schema(operation_summary = "Retrieve a comment")
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+    
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+    
+    @swagger_auto_schema(method = "post", operation_summary = "Reply on a comment")
+    @action(detail = True, methods = ["POST"], permission_classes = [permissions.IsAuthenticated])
+    def reply(self, request: HttpRequest, pk = None):
+        request.data
+        serializer = self.get_serializer(data = request.data,
+                                         context = {
+                                             "parent_id" : pk,
+                                             "user_id": request.user.id
+                                         })
+        serializer.is_valid(raise_exception = True)
         instance = serializer.save()
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status = status.HTTP_201_CREATED, headers = headers)
+    
