ec575c24bb0fdb8b41b78f4169836c84259d7ed8
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-14 21:45:53 +0330

    refactor(comment-viewset): general refactoring + implement reply

--- src/api/views/CommentViewSet.py
+++ src/api/views/CommentViewSet.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.db.models import F
 from rest_framework import viewsets
 from rest_framework import parsers
 from rest_framework import status
@@ -19,8 +20,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     pagination_class = pagination.CustomPagination
 
     serializer_action_classes = {
-        "create" : serializers.CreateCommentSerializer,
-        "reply" : serializers.CreateCommentSerializer,
+        "list": serializers.CommentListSerializer,
+        "retrieve": serializers.CommentListSerializer,
+        "create": serializers.CreateCommentSerializer,
+        "update": serializers.CreateCommentSerializer,
+        "partial_update": serializers.CreateCommentSerializer,
+        "reply": serializers.CreateCommentSerializer,
+        "like": None,
     }
 
     def get_serializer_class(self):
@@ -53,13 +59,47 @@ class CommentViewSet(viewsets.ModelViewSet):
         return super().retrieve(request, *args, **kwargs)
     
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
-    
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials were not provided"},
+                            status = status.HTTP_401_UNAUTHORIZED)
+        
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+
+        if instance.user_id != request.user.id:
+            return Response({"detail": "operation not allowed, you are not the creator of this comment"},
+                            status = status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
     def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
-    
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials were not provided"},
+                            status = status.HTTP_401_UNAUTHORIZED)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
     def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
+        if not request.user.is_authenticated:
+            return Response({"detail": "Authentication credentials were not provided"},
+                            status = status.HTTP_401_UNAUTHORIZED)
+        instance = self.get_object()
+
+        if instance.user_id != request.user.id:
+            return Response({"detail": "operation not allowed, you are not the creator of this comment"},
+                        status = status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
     @swagger_auto_schema(method = "post", operation_summary = "Reply on a comment")
     @action(detail = True, methods = ["POST"], permission_classes = [permissions.IsAuthenticated])
@@ -74,4 +114,11 @@ class CommentViewSet(viewsets.ModelViewSet):
         instance = serializer.save()
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status = status.HTTP_201_CREATED, headers = headers)
+    
+    @swagger_auto_schema(method = "post", operation_summary = "Like a comment")
+    @action(detail = True, methods = ["POST"], permission_classes =  [permissions.IsAuthenticated])
+    def like(self, request, pk = None):
+        queryset = self.get_queryset()
+        queryset.filter(pk = pk).update(score = F("score") + 1)
+        return Response(status = status.HTTP_204_NO_CONTENT)
     
