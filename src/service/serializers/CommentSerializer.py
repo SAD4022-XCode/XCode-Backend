5fd184ab429a11dd4d9a965e2fe5fda32356ae92
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-15 19:33:32 +0330

    feat(comment-serializer): display reply list and flag

--- src/service/serializers/CommentSerializer.py
+++ src/service/serializers/CommentSerializer.py
@@ -9,6 +9,8 @@ class CommentSerializer(serializers.ModelSerializer):
     parent = serializers.PrimaryKeyRelatedField(queryset = models.Comment.objects.all())
     user_photo = serializers.ImageField(source = "user.userprofile.profile_picture")
     username = serializers.CharField(source = "user.username")
+    liked_by = serializers.PrimaryKeyRelatedField(queryset = models.User.objects.all(),
+                                                  many = True)
 
     class Meta:
         model = models.Comment
@@ -23,10 +25,24 @@ class CommentSerializer(serializers.ModelSerializer):
             "created_at",
             "score",
             "text",
+            "liked_by",
         ]
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        serializer = CommentSerializer(instance.children.prefetch_related("children").all(), many = True)
+        request_user = self.context.get("request_user")
+
+        if (request_user is not None and instance.liked_by.filter(pk = request_user)):
+            representation["has_liked"] = True
+        else:
+            representation["has_liked"] = False
+        
+        serializer = CommentSerializer(instance.children \
+                                        .prefetch_related("children", "liked_by") \
+                                        .select_related("user", "event") \
+                                        .all(), 
+                                       many = True, 
+                                       context = self.context)
         representation["replies"] = serializer.data
+        
         return representation
