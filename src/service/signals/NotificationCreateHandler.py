1ca756917c79cef0438c7634124d75ddef8c002b
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-22 20:54:56 +0330

    feat(notification): implement send notification on new message receipt

---
+++ src/service/signals/NotificationCreateHandler.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from data import models
+
+class PrivateMessage:
+    pass
+
+@receiver(post_save, sender = PrivateMessage)
+def new_private_message_notification(sender, instance, created, **kwargs):
+    if not created:
+        return
+    models.Notification.objects.create(recipient_id = sender.recipient_id,
+                                       title = "new message",
+                                       content = f"you have a new message from {sender.sender_id}")    
