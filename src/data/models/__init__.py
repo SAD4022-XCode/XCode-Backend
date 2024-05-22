8b1d9342d3b25ae09730669b0da071c9fed69e31
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-22 20:41:47 +0330

    chore: import modules

--- src/data/models/__init__.py
+++ src/data/models/__init__.py
@@ -3,6 +3,7 @@ from data.models.UserProfile import UserProfile
 from data.models.Event import Event, OnlineEvent, InPersonEvent
 from data.models.Tag import Tag
 from data.models.Comment import Comment
+from data.models.Notification import Notification
 
 __all__ = [
     User,
@@ -12,4 +13,5 @@ __all__ = [
     InPersonEvent,
     Tag,
     Comment,
+    Notification,
 ]
--- src/service/signals/__init__.py
+++ src/service/signals/__init__.py
@@ -1,1 +1,7 @@
-from .UserProfileCreateHandler import UserProfileCreateHandler
+from service.signals.UserProfileCreateHandler import user_profile_create_handler
+from service.signals.NotificationCreateHandler import new_private_message_notification
+
+__all__ = [
+    user_profile_create_handler,
+    new_private_message_notification,
+]
