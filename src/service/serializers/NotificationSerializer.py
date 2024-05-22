5d6fb742bae8db34edb7f98263631fd6feb458b7
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-22 20:52:08 +0330

    feat(notification): implement notification serializer

---
+++ src/service/serializers/NotificationSerializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from data import models
+
+class NotificationSerializer(serializers.ModelSerializer):
+    recipient = serializers.PrimaryKeyRelatedField(queryset = models.UserProfile.objects.all())
+
+    class Meta:
+        model = models.Notification
+
+        fields = [
+            "id",
+            "recipient",
+            "title",
+            "content",
+            "time",
+            "is_read",
+        ]
