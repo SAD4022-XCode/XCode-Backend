f54a94833429a0c22791b55d2ae9cbbad1f517d2
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-22 20:51:39 +0330

    feat(notification): implement notification data model

---
+++ src/data/models/Notification.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from data import models as AppModels
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(AppModels.UserProfile,
+                                  on_delete = models.CASCADE)
+    time = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    is_read = models.BooleanField(default = False)
