db6f43f68ef6f2a06f9b32dc17e668d00107d752
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-24 21:46:11 +0330

    feat(notification): mark as read api + general refactoring

--- src/api/views/NotificationViewSet.py
+++ src/api/views/NotificationViewSet.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 from rest_framework import parsers
 from rest_framework import mixins
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from drf_yasg.utils import swagger_auto_schema
 
 from data import models
 from service import serializers
@@ -20,12 +24,25 @@ class NotificationViewSet(mixins.ListModelMixin,
         parsers.MultiPartParser,
     ]
 
+    serializer_action_classes = {
+        "list": serializer_class,
+        "mark_as_read": None,
+    }
+
+    def get_serializer_class(self):
+        try:
+            return self.serializer_action_classes[self.action]
+        except(KeyError, AttributeError):
+            return super().get_serializer_class()
+
+    @swagger_auto_schema(operation_summary = "List of all notifications")
     def list(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return Response({"detail": "Authentication credentials were not provided"},
                             status = status.HTTP_401_UNAUTHORIZED)
 
-        queryset = self.get_queryset().filter(recipient_id = request.user.id)
+        # queryset = self.get_queryset().filter(recipient_id = request.user.id)
+        queryset = self.filter_queryset(self.get_queryset())
 
         page = self.paginate_queryset(queryset)
         if page is not None:
@@ -35,6 +52,7 @@ class NotificationViewSet(mixins.ListModelMixin,
         serializer = self.get_serializer(queryset, many = True)
         return Response(serializer.data)
     
+    @swagger_auto_schema(operation_summary = "Delete a notification (login required)")
     def destroy(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return Response({"detail": "Authentication credentials were not provided"},
@@ -48,3 +66,19 @@ class NotificationViewSet(mixins.ListModelMixin,
 
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @swagger_auto_schema(method = "patch", operation_summary = "Mark notification as read (login required)")
+    @action(detail = True, methods = ["PATCH"], permission_classes = [permissions.IsAuthenticated])
+    def mark_as_read(self, request, pk = None):
+        instance = self.get_object()
+
+        if instance.recipient_id != request.user.id:
+            return Response({"detail": "operation not allowed, you are not the recipient of this notification"}, 
+                            status = status.HTTP_403_FORBIDDEN)
+        
+        if instance.is_read == True:
+            return Response(status = status.HTTP_204_NO_CONTENT)
+        instance.is_read = True;        
+        instance.save(update_fields = ["is_read"])
+
+        return Response(status = status.HTTP_204_NO_CONTENT)
