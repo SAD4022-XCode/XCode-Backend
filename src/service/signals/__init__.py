d107c345ed27cab2e473e715e00883b709ca1973
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-05-29 23:22:21 +0330

    refactor: remove unnecessary code

--- src/service/signals/__init__.py
+++ src/service/signals/__init__.py
@@ -1,7 +1,5 @@
 from service.signals.UserProfileCreateHandler import user_profile_create_handler
-from service.signals.NotificationCreateHandler import new_private_message_notification
 
 __all__ = [
     user_profile_create_handler,
-    new_private_message_notification,
 ]
