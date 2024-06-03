48187537d58eb21e47da84f8690655290f09230a
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-06-03 19:59:35 +0330

    refactor(event-details): add required fields

--- src/service/serializers/event_serializers/EventDetailSerializer.py
+++ src/service/serializers/event_serializers/EventDetailSerializer.py
@@ -7,6 +7,9 @@ from data import models
 class EventDetailSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only = True)
     creator_id = serializers.PrimaryKeyRelatedField(queryset = models.User.objects.all(), required = False)
+    organizer_name = serializers.CharField(source = "creator.first_name")
+    organizer_photo = serializers.ImageField(source = "creator.userprofile.profile_picture")
+    organizer_email = serializers.CharField(source = "creator.email")
     remaining_tickets = serializers.IntegerField(read_only = True, required = False)
     inpersonevent = InPersonEventSerializer()
     onlineevent = OnlineEventSerializer()
@@ -28,6 +31,9 @@ class EventDetailSerializer(serializers.ModelSerializer):
             "remaining_tickets",
             "is_paid",
             "ticket_price",
+            "organizer_name",
+            "organizer_photo",
+            "organizer_email",
             "organizer_phone",
             "organizer_SSN",
             "photo",
