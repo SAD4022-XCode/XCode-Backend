4a83013df97617b0ffec87124fd4f30283753d2c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-06-20 15:54:47 +0330

    feat(user-profile): add enrolled events field

--- src/data/models/UserProfile.py
+++ src/data/models/UserProfile.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.conf import settings
+from django.core import exceptions
 from pathlib import Path
 
-from .managers import UserProfileManager
+from data.models.managers import UserProfileManager
+from data.models.Event import Event
 
 GENDER_MALE = 'M'
 GENDER_FEMALE = 'F'
@@ -29,6 +31,8 @@ class UserProfile(models.Model):
     gender = models.CharField(max_length = 1, 
                               choices = GENDER_CHOICES, 
                               default = GENDER_UNKNOWN)
+    enrolled_events = models.ManyToManyField(Event, through = "Ticket")
+    has_enrolled = models.BooleanField(default = False)
     
     objects = UserProfileManager()
 
@@ -37,3 +41,13 @@ class UserProfile(models.Model):
     
     def delete_profile_picture(self):
         self.profile_picture.delete(save = True)
+
+    def pay(self, price):
+        if (self.balance < price):
+            raise exceptions.BadRequest
+        self.balance -= price
+        return self.balance
+    
+    def deposit(self, value):
+        self.balance += value
+        return self.balance
