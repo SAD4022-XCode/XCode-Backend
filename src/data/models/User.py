6526582e5c3dc70d18e18af945c1ee9c5dd163a1
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-06-28 00:04:57 +0330

    feat: add conversations field

--- src/data/models/User.py
+++ src/data/models/User.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique = True)
+    conversations = models.ManyToManyField("Conversation", through = "UserConversation")
