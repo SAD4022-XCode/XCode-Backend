5701f5a65104c11cc94ddfaf539d505b967e49b9
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-07-01 00:33:13 +0330

    refactor: add, modify fields

--- src/data/models/Message.py
+++ src/data/models/Message.py
@@ -4,6 +4,9 @@ from data import models as AppModels
 
 class Message(models.Model):
     sender = models.ForeignKey(AppModels.User, on_delete = models.CASCADE)
-    conversation = models.ForeignKey(AppModels.Conversation, on_delete = models.CASCADE)
+    conversation = models.ForeignKey(AppModels.Conversation, 
+                                     on_delete = models.CASCADE, 
+                                     related_name = "message_history")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add = True)
+    is_read = models.BooleanField(default = False)
--- src/data/models/User.py
+++ src/data/models/User.py
@@ -3,4 +3,6 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique = True)
-    conversations = models.ManyToManyField("Conversation", through = "UserConversation")
+    conversations = models.ManyToManyField("Conversation", 
+                                           through = "UserConversation", 
+                                           related_name = "participants")
