621abc5d3081dfbda39307c3f47e428f7f47b6d3
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-07-03 01:43:40 +0330

    feat: implement Message serializer

--- src/service/serializers/MessageSerializer.py
+++ src/service/serializers/MessageSerializer.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from data import models
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = serializers.PrimaryKeyRelatedField(queryset = models.User.objects.all())
+    sender = serializers.PrimaryKeyRelatedField(queryset = models.User.objects.select_related("userprofile").all())
     sender_name = serializers.CharField(source = "sender.first_name")
     sender_profile_photo = serializers.ImageField(source = "sender.userprofile.profile_picture")
     conversation = serializers.PrimaryKeyRelatedField(queryset = models.Conversation.objects.all())
@@ -13,6 +13,7 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = [
             "sender",
             "sender_name",
+            "sender_profile_photo",
             "conversation",
             "content",
             "timestamp",
