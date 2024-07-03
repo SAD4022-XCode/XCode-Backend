1dfbe71b285b234cbe51aced080364639da0e1ef
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-07-03 17:17:53 +0330

    refactor: general refactoring

--- src/api/views/ConversationViewSet.py
+++ src/api/views/ConversationViewSet.py
@@ -4,12 +4,13 @@ from rest_framework import parsers
 from rest_framework import decorators
 from rest_framework import permissions
 from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
 from django.db import transaction
 
 from data import models
 from service import serializers
-class ConversationViewSet(mixins.ListModelMixin,
-                        viewsets.GenericViewSet):
+class ConversationViewSet(viewsets.GenericViewSet):
     queryset = models.Conversation.objects \
         .prefetch_related("participants", "message_history") \
         .all()
@@ -30,14 +31,14 @@ class ConversationViewSet(mixins.ListModelMixin,
             return self.serializer_action_classes[self.action]
         except(KeyError, AttributeError):
             return super().get_serializer_class()
-
-    @transaction.atomic
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
     
     @decorators.action(detail = True, methods = ["GET"], permission_classes = [permissions.IsAuthenticated])
     @transaction.atomic
     def message_history(self, request, pk = None):
+        if not models.UserConversation.objects \
+            .filter(Q(user_id = request.user.id) & Q(conversation_id = pk)) \
+            .exists():
+            return Response({"detail": "Forbidden"}, status = status.HTTP_403_FORBIDDEN)
         conversation = self.get_object()
         messages = conversation.message_history
         serializer = serializers.MessageSerializer(messages, 
